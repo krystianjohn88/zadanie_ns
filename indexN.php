a7b8112dad2c015fffec686b12f59fefa3244ba1
krystianjohn88 <36715049+krystianjohn88@users.noreply.github.com>
2020-05-16 18:39:35 +0200

    kasowanie cd
    wykasowalem wszystko co nie potrzebne

--- indexN.php
+++ indexN.php
@@ -1,7 +1,7 @@
 <?php
 
 function poszukiwacz(string $swiat,  $index): int{
-    if($index== null){
+    if($index == null || $index == '' || $index<-1 || $index>strlen($swiat)){
         $index=-1;
     }
     /* tutaj ciało funkcji*/
@@ -27,7 +27,7 @@ function poszukiwacz(string $swiat,  $index): int{
         } 
     }
     $i=0;
-    while($i <count($skarb)){
+    while($i<count($skarb)){
         array_push($iSkarb, $skarb[$i][0]);
         $i++;
     }
@@ -35,7 +35,7 @@ function poszukiwacz(string $swiat,  $index): int{
     for($i=0; $i<count($skarb); $i++){
         if($skarb[$i][0]==array_keys($iSkarb, max($iSkarb))[0]){
             $index = $skarb[$i][1];
-            $i=count($skarb);
+            break;
         }
     }
     return $index;
@@ -43,10 +43,12 @@ function poszukiwacz(string $swiat,  $index): int{
 $arrTest = [];
 $arrTest[] = ["cave"=>"(((*))(((((*)))(*))))",'index'=> 3];
 $arrTest[] = ["cave"=>"(((**))(((((*)))(***))))(((*",'index'=> 4];
-$arrTest[] = ["cave"=>"(((*))(((((***)))(*******))))(((*",'index'=> 22];
+$arrTest[] = ["cave"=>"(((*))(((((***)))(*******))))(((*",'index'=> 220];
 $arrTest[] = ["cave"=>"(((****))(((((***)))(*))))(((*",'index'=> 10];
 $arrTest[] = ["cave"=>"",'index'=> null];
+$arrTest[] = ["cave"=>"(((****))(((((***))",'index'=> ''];
 
 foreach($arrTest as $t){
-    echo poszukiwacz($t['cave'], $t['index']).' => index poczatkowy: '.$t['index'].PHP_EOL; //zmodifikowałem tak aby funckja przyjeła też index
+    echo poszukiwacz($t['cave'], $t['index']).' => index poczatkowy: '.$t['index'].PHP_EOL; //zmodifikowałem tak aby funckja przyjeła też index początkowy
 }
+?>
